6461407f59e25f7fd1fda57ceabb6af8aa951bf4
Yash Sharma <yash8987rs@gmail.com>
2024-05-07 17:03:13 +0530

    minor refactor

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div
       name="about"
-      className="w-full h-screen bg-gradient-to-b from bg-gray-800 to-black text-white"
+      className="w-full h-[120vh] bg-gradient-to-b from bg-gray-800 to-black text-white md:h-screen"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,7 @@ import {Link} from "react-scroll";
 
 const Home = () => {
   return (
-    <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
+    <div name="home" className='h-[120vh] w-full bg-gradient-to-b from-black via-black to-gray-800 md:h-screen'>
       <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
         <div className='flex flex-col justify-center h-full'>
           <h2 className='text-4xl md:text-7xl font-bold text-white'>I'm a Full Stack Developer</h2>
