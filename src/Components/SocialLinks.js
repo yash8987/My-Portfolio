ea0120fe7acdedc58c0b5089394717a5bc30d572
Yash Sharma <yash8987rs@gmail.com>
2024-05-08 15:59:26 +0530

    minor refactor

--- src/Components/SocialLinks.js
+++ src/Components/SocialLinks.js
@@ -5,7 +5,7 @@ import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const SocialLinks = () => {
   return (
-    <div className='hidden lg:flex flex-col top-[35%] left-0 fixed'>
+    <div className='hidden xl:flex flex-col top-[35%] left-0 fixed '>
       <ul>
         <li className='flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500'>
             <a href="https://www.linkedin.com/in/yashsharma8987/" target='_blank' rel="noreferrer" className='flex justify-between items-center w-full text-white'>
--- src/Components/SocialLinksSmall.js
+++ src/Components/SocialLinksSmall.js
@@ -5,7 +5,7 @@ import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const SocialLinksSmall = () => {
   return (
-    <div className='flex justify-center items-center bg-gradient-to-b from bg-gray-800 to-black w-full h-[20vh] gap-9 lg:hidden'>
+    <div className='flex justify-center bg-gradient-to-b from bg-gray-800 to-black w-full h-[20vh] gap-9 xl:hidden'>
       <ul>
         <li className='flex justify-between items-center rounded-md bg-gray-500'>
             <a href="https://www.linkedin.com/in/yashsharma8987/" target='_blank' rel="noreferrer" className='flex justify-between items-center w-full text-white'>
